4b527d626a3c15e2ecde80f0bb2bfa323f94928b
amp <amp834@rqinc.com>
2013-07-16 20:12:41 -0400

    Initial setup with controller and images

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Amp01::Application.routes.draw do
+  root :to => 'pages#home'
+
+  get "pages/home"
+
+  get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
